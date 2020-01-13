8e9a9621f20da0b9c2824ab8cbfbea02b2ef6c80
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-01-13 13:45:39 +1100

    added comments to main.js

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,11 @@
-console.log("Up and running!");
 
+// declare four variables; one for each card
 let cardOne = "queen";
 let cardTwo = "queen";
 let cardThree = "king";
 let cardFour = "king";
 
+
+// human-readable code for console
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
