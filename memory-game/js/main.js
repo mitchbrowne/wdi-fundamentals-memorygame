d1f6e44b84aed99852b6b03441fa97250ca70bf1
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-01-14 12:18:07 +1100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,11 +1,34 @@
 
-// declare four variables; one for each card
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+// declare an array called cards; add an item for each card
+let cards = ["queen", "queen", "king", "king"];
 
+// declare an array called cardsInPlay; each item will eventually hold the cards in play
+let cardsInPlay = [];
 
-// human-readable code for console
+// declare variable cardOne; represents first card player has flipped
+let cardOne = cards[0];
+
+// add first card to array cardsInPlay
+cardsInPlay.push(cardOne);
+
+// display card that was just flipped in console
 console.log("User flipped " + cardOne);
+
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
 console.log("User flipped " + cardTwo);
+
+// condition to ensure user has picked two cards before code continues
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		// cards are equal
+		alert("You found a match!");
+	}
+	else {
+		// cards are not equal
+		alert("Sorry, try again!");
+	}
+}
+
